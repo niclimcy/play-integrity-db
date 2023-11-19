74bf7a1201ae9e2d174f8b6dfc8647932b5fa808
Nicholas Lim <nicknitewolf@lineageos.org>
2023-11-19 21:42:42 +0800

    SearchBar: Remove help button to SearchBar component

--- src/App.vue
+++ src/App.vue
@@ -16,35 +16,7 @@ const handleQuery = (value: string) => {
       <img src="/favicon/favicon.svg" class="img" alt="Play Integrity DB" style="width: 4rem">
       <h1 class="display-4">Play Integrity DB</h1>
     </div>
-    <div class="mt-4 g-2">
-      <SearchBar @query="handleQuery" />
-    </div>
-    <div class="collapse mt-4" id="collapseInfo">
-      <div class="card">
-        <ul class="list-group list-group-flush">
-          <li class="list-group-item bg-success-subtle d-flex align-items-center">
-            <span class="material-symbols-outlined me-2">check_circle</span>
-            No checks - No known checks that prevent app usage.
-          </li>
-          <li class="list-group-item bg-primary-subtle d-flex align-items-center">
-            <span class="material-symbols-outlined me-2">info</span>
-            Root Check - Checks if the device is rooted.
-          </li>
-          <li class="list-group-item bg-warning-subtle d-flex align-items-center">
-            <span class="material-symbols-outlined me-2">warning</span>
-            Device integrity - Checks if device's CTS profile matches.
-          </li>
-          <li class="list-group-item bg-danger-subtle d-flex align-items-center">
-            <span class="material-symbols-outlined me-2">dangerous</span>
-            Strong integrity - Does a hardware backed attestation check.
-          </li>
-          <li class="list-group-item bg-danger-subtle d-flex align-items-center">
-            <span class="material-symbols-outlined me-2">dangerous</span>
-            Bootloader Check - Checks if the device's bootloader is unlocked.
-          </li>
-        </ul>
-      </div>
-    </div>
+    <SearchBar class="mt-4 g-2" @query="handleQuery" />
     <AppList class="mt-4" :query="query" />
     <div v-if="query" class="alert alert-info alert-dismissible fade show mt-4" role="alert">
       <h4 class="alert-heading d-flex align-items-center">
--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -20,7 +20,34 @@ const query = ref('')
     </div>
     <button class="btn btn-outline-secondary d-flex align-items-center" type="button" data-bs-toggle="collapse"
       data-bs-target="#collapseInfo" aria-expanded="false" aria-controls="collapseInfo">
-      <span class="material-symbols-outlined">info</span>
+      <span class="material-symbols-outlined">help</span>
     </button>
+  </div>
+
+  <div class="collapse mt-4" id="collapseInfo">
+    <div class="card">
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item bg-success-subtle d-flex align-items-center">
+          <span class="material-symbols-outlined me-2">check_circle</span>
+          No checks - No known checks that prevent app usage.
+        </li>
+        <li class="list-group-item bg-primary-subtle d-flex align-items-center">
+          <span class="material-symbols-outlined me-2">info</span>
+          Root Check - Checks if the device is rooted.
+        </li>
+        <li class="list-group-item bg-warning-subtle d-flex align-items-center">
+          <span class="material-symbols-outlined me-2">warning</span>
+          Device integrity - Checks if device's CTS profile matches.
+        </li>
+        <li class="list-group-item bg-danger-subtle d-flex align-items-center">
+          <span class="material-symbols-outlined me-2">dangerous</span>
+          Strong integrity - Does a hardware backed attestation check.
+        </li>
+        <li class="list-group-item bg-danger-subtle d-flex align-items-center">
+          <span class="material-symbols-outlined me-2">dangerous</span>
+          Bootloader Check - Checks if the device's bootloader is unlocked.
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
